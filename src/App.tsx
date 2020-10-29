3c1856c4fa5257d9fa1f93e9ec6e5a86d2e4ce8b
Nihad <ngazic@gmail.com>
2020-10-29 04:53:54 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+const App: React.FunctionComponent = () => {
+  return (<div>
+    <h1>Welcome to cool new App.</h1>
+  </div>);
+};
+
+export default App;

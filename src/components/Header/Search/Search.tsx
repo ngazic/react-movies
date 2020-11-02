eabe1f6bdaa4213cbd3ea961e09d3effb4eaa635
ngazic <ngazic@gmail.com>
2020-11-02 15:13:30 +0100

    deleted store for reinitialization

--- src/components/Header/Search/Search.tsx
+++ src/components/Header/Search/Search.tsx
@@ -1,14 +1,12 @@
 import React, { FormEvent } from 'react'
 import { useDispatch } from 'react-redux'
 import './Search.scss'
-import { searchFor } from '../../../store/actions/searchActions'
 
 const Search: React.FC = (props?) => {
   const dispatch = useDispatch();
 
   const changeHandler = (e: FormEvent<HTMLInputElement>) => {
     console.log('CHANGE')
-    dispatch(searchFor(e.currentTarget.value))
   }
 
 
